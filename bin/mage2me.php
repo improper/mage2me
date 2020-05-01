7303b868f37888cd571db3bd98f5826c8568e3a6
Tyler Mills <tylerssn@gmail.com>
2020-05-01 17:41:20 -0400

    Release first version of mage2me

--- bin/mage2me.php
+++ bin/mage2me.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\DownloadCommand;
 use Symfony\Component\Console\Application;
 
-$application = new Application('mage2me', 'v0.0.0-beta6');
+$application = new Application('mage2me', '1.0.0');
 $application->add(new DownloadCommand());
 $application->run();
 
