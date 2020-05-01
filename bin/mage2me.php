5b4d99425bdc81614cc80355c7773ca9c51fddc6
Tyler Mills <tylerssn@gmail.com>
2020-05-01 17:18:38 -0400

    Test gitlab releases when version matches correctly

--- bin/mage2me.php
+++ bin/mage2me.php
@@ -5,7 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use App\DownloadCommand;
 use Symfony\Component\Console\Application;
 
-$application = new Application('mage2me', '1.0.0');
+$application = new Application('mage2me', 'v1.0.0-test2');
 $application->add(new DownloadCommand());
 $application->run();
 
